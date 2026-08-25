3509e219e7ebfdebd8e93159bd806e05da9073c6
Luis Miguel Ponce Herrera <zlagustin10@gmail.com>
2026-08-24 20:15:53 -0600

    style: Remove time from acta date display

--- js/historial.js
+++ js/historial.js
@@ -47,9 +47,7 @@ class HistorialModule {
             weekday: 'long', 
             year: 'numeric', 
             month: 'long', 
-            day: 'numeric',
-            hour: '2-digit',
-            minute: '2-digit'
+            day: 'numeric'
         });
     }
 
